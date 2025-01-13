17c08a9176119b44ac642c859c30419b56c4f903
Ryan <82134428+ryanrixxh@users.noreply.github.com>
2025-01-13 19:19:33 +1030

    adding state machine and interactable vine
    Added a state machine to track the activities of the player character and respond without using a bunch of if statements.
    
    Added an interactable area on the vine with monitoring. Start of the grabbing feature.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 @export var speed = 400
-@export var acceleration = 1000
-@export var jump_velocity = -1000
-@export var gravity = 1000
+@export var acceleration = 2000
+@export var jump_velocity = -850
+@export var gravity = 1500
+
+# State Machine
+enum States {GROUNDED, AIRBORN, SWINGING}
+var state: States = States.GROUNDED
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,20 +21,27 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	get_horizontal_movement(delta)
-
 		
 	if not is_on_floor():
+		state = States.AIRBORN
 		velocity.y += gravity * delta
+	else:
+		state = States.GROUNDED
 		
 	if Input.is_action_just_pressed("jump"):
-		velocity.y = jump_velocity
+		jump()
 		
 	move_and_slide()
+
 	
 func get_horizontal_movement(delta: float):
 	var direction = Input.get_axis("left", "right")	
 	var arial_acceleration = acceleration / 2
-	velocity.x = move_toward(velocity.x, direction * speed, (acceleration if is_on_floor() else arial_acceleration)  * delta)
+	velocity.x = move_toward(velocity.x, direction * speed, (acceleration if state == States.GROUNDED else arial_acceleration)  * delta)
 	
 func jump():
 	velocity.y = jump_velocity
+
+
+func _on_interactable_body_entered(body: Node2D) -> void:
+	print('vine grabbable!')

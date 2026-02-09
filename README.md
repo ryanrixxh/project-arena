26d651486eefaf8bf9f9cd8bb5a0033814a752e8
Ryan <82134428+ryanrixxh@users.noreply.github.com>
2026-02-09 17:33:37 +1030

    ive changed the is_on_floor() function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project-vine
 Godot vine swinging project
+
+## TODOS
+	- Vines need to be affected by physics. Right now they are simply nodes and their interactable areas have all the script logic. This needs to be changed, the parent Vine needs to be the Rigid body that rotates on force applied, and the interactable need to maintain responsibility of the handling of player interaction and grabbing.
--- scripts/player.gd
+++ scripts/player.gd
@@ -24,30 +24,30 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		jump()
-	
+
 	if state == States.SWINGING:
 		global_position = current_vine.get_node("GrabMarker").global_position
 		move_and_slide()
-		return 
-		
+		return
+
 	get_horizontal_movement(delta)
-		
-	if not is_on_floor():
+
+	if not is_on_floor_2():
 		state = States.AIRBORN
 		velocity.y += gravity * delta
 	else:
 		state = States.GROUNDED
-		
+
 	move_and_slide()
 
 
-	
+
 func get_horizontal_movement(delta: float):
-	var direction = Input.get_axis("left", "right")	
+	var direction = Input.get_axis("left", "right")
 	var arial_acceleration = acceleration / 2
 	velocity.x = move_toward(velocity.x, direction * speed, (acceleration if state == States.GROUNDED else arial_acceleration)  * delta)
-	
-	
+
+
 func jump():
 	state = States.AIRBORN
 	velocity.y = jump_velocity

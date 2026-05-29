5b519168e358ef36685c707918a3cd829f982936
ryanrixxh <ryanmbr@pm.me>
2026-05-29 13:58:39 +0930

    adding a damage calculation to the damage component based on a multiplier. Higher multi = more damage based on speed of travel

--- scripts/components/damage.gd
+++ scripts/components/damage.gd
@@ -1,7 +1,7 @@
 class_name Damage extends Node
 
 @export var pickup_body: RigidBody2D
-@export var damage: int
+@export var damage_multiplier: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,13 +12,14 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
+func calculate_damage() -> int:
+	return (abs(pickup_body.linear_velocity[0]) + abs(pickup_body.linear_velocity[1])) / 10
 
 func _on_pickup_body_entered(body: Node) -> void:
 	var index = body.get_children().find_custom(func(child: Node): return child is Health)
 	if (index != -1):
-		do_damage(body.get_child(index))
+		do_damage(calculate_damage(), body.get_child(index))
 
-func do_damage(health_component: Health) -> void:
-	print("Doing damage to ", health_component)
+func do_damage(damage: int, health_component: Health) -> void:
 	health_component.health -= damage
 	health_component.check_health()
